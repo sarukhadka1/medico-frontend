6a8201ef83d4fbf4725476165792b4f236da6b5e
Saru <saru.khadka521@gmail.com>
2025-01-22 17:40:04 +0545

    logo added

--- src/components/AdminNavbar.jsx
+++ src/components/AdminNavbar.jsx
@@ -17,7 +17,9 @@ const AdminNavbar = () => {
     <div className='container-fluid p-0'>
       <nav className="navbar navbar-expand-lg bg-body-tertiary">
         <div className="container-fluid">
-          <a className="navbar-brand" href="#">Medico</a>
+          <a className="navbar-brand" href="#">
+          <img src="/assets/images/medico1.png" alt="" className="navbar-logo" />
+          </a>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
